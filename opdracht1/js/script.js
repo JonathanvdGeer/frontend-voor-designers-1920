4c39760161849890d20d549619bbe41a0f88e4a8
JonathanvdGeer <jona.vd.geer@hotmail.com>
2020-02-06 18:29:52 +0100

    Versiew 2.0

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -4,3 +4,46 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill Listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop Through Empties and call drag events
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragover', dragEnter);
+    empty.addEventListener('dragover', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += 'hovered';
+}
+
+function dragLeave() {
+    this.className = 'empty';
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+}
